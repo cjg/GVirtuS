623909773178d4c4a3f7216a89db0109a829b403
Giuseppe Coviello <gcov@pm.me>
2009-09-30 12:56:02 +0200

    initial import

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,24 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX
+CND_ARTIFACT_NAME_Debug=libEchoesFrontend.dylib
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/libEchoesFrontend.dylib
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
+CND_PACKAGE_NAME_Debug=libEchoesFrontend.dylib.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/libEchoesFrontend.dylib.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX
+CND_ARTIFACT_NAME_Release=libEchoesFrontend.dylib
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-MacOSX/libEchoesFrontend.dylib
+CND_PACKAGE_DIR_Release=dist/Release/GNU-MacOSX/package
+CND_PACKAGE_NAME_Release=libEchoesFrontend.dylib.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-MacOSX/package/libEchoesFrontend.dylib.tar
