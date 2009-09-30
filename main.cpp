b295a8ccc8a4b04614ac9604630b69f9f7a9b764
Giuseppe Coviello <gcov@pm.me>
2009-09-30 12:23:17 +0200

    initial import

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.cpp
+ * Author: cjg
+ *
+ * Created on 30 settembre 2009, 12.21
+ */
+
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
