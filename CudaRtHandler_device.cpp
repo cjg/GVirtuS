6717bfb01fe3daed345e4be19974467c8daef609
Giuseppe Coviello <gcov@pm.me>
2009-10-27 10:52:56 +0100

    splitting routine handler: created CudaRtHandler_device

---
+++ CudaRtHandler_device.cpp
@@ -0,0 +1,24 @@
+#include <cuda_runtime_api.h>
+#include "CudaRtHandler.h"
+
+CUDA_ROUTINE_HANDLER(GetDeviceCount) {
+    /* cudaError_t cudaGetDeviceCount(int *count) */
+
+    int *count = input_buffer->Assign<int>();
+
+    cudaError_t exit_code = cudaGetDeviceCount(count);
+
+    return new Result(exit_code, new Buffer(*input_buffer));
+}
+
+CUDA_ROUTINE_HANDLER(GetDeviceProperties) {
+    /* cudaError_t cudaGetDeviceCount(struct cudaDeviceProp *prop,
+       int device) */
+    struct cudaDeviceProp *prop = input_buffer->Assign<struct cudaDeviceProp>();
+    int *device = input_buffer->Assign<int>();
+
+    cudaError_t exit_code = cudaGetDeviceProperties(prop, *device);
+
+    return new Result(exit_code, new Buffer(*input_buffer));
+}
+
