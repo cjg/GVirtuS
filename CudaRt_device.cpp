11dbe32fa913a20a596a5bdd22b30d848f27a345
Giuseppe Coviello <gcov@pm.me>
2009-11-09 12:39:19 +0100

    implemented device routine handler

--- CudaRt_device.cpp
+++ CudaRt_device.cpp
@@ -6,15 +6,22 @@
 using namespace std;
 
 extern cudaError_t cudaChooseDevice(int *device, const cudaDeviceProp *prop) {
-    /* FIXME: implement */
-    cerr << "*** Error: cudaChooseDevice() not yet implemented!" << endl;
-    return cudaErrorUnknown;
+    Frontend *f = Frontend::GetFrontend();
+    f->AddHostPointerForArguments(device);
+    f->AddHostPointerForArguments(prop);
+    f->Execute("cudaChooseDevice");
+    if(f->Success())
+        *device = *(f->GetOutputHostPointer<int>());
+    return f->GetExitCode();
 }
 
 extern cudaError_t cudaGetDevice(int *device) {
-    /* FIXME: implement */
-    cerr << "*** Error: cudaGetDevice() not yet implemented!" << endl;
-    return cudaErrorUnknown;
+    Frontend *f = Frontend::GetFrontend();
+    f->AddHostPointerForArguments(device);
+    f->Execute("cudaGetDevice");
+    if(f->Success())
+        *device = *(f->GetOutputHostPointer<int>());
+    return f->GetExitCode();
 }
 
 extern cudaError_t cudaGetDeviceCount(int *count) {
@@ -44,13 +51,20 @@ extern cudaError_t cudaSetDevice(int device) {
 }
 
 extern cudaError_t cudaSetDeviceFlags(int flags) {
-    /* FIXME: implement */
-    cerr << "*** Error: cudaSetDeviceFlags() not yet implemented!" << endl;
-    return cudaErrorUnknown;
+    Frontend *f = Frontend::GetFrontend();
+    f->AddVariableForArguments(flags);
+    f->Execute("cudaSetDeviceFlags");
+    return f->GetExitCode();
 }
 
 extern cudaError_t cudaSetValidDevices(int *device_arr, int len) {
-    /* FIXME: implement */
-    cerr << "*** Error: cudaSetValidDevices() not yet implemented!" << endl;
-    return cudaErrorUnknown;
+    Frontend *f = Frontend::GetFrontend();
+    f->AddHostPointerForArguments(device_arr, len);
+    f->AddVariableForArguments(len);
+    f->Execute("cudaSetValidDevices");
+    if(f->Success()) {
+        int *out_device_arr = f->GetOutputHostPointer<int>();
+        memmove(device_arr, out_device_arr, sizeof(int) * len);
+    }
+    return f->GetExitCode();
 }
