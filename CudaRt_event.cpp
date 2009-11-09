aa116c822adcdce75145a79618f1ab2dd42d49f5
Giuseppe Coviello <gcov@pm.me>
2009-11-09 13:25:20 +0100

    implemented event routine handler

--- CudaRt_event.cpp
+++ CudaRt_event.cpp
@@ -4,44 +4,60 @@
 using namespace std;
 
 extern cudaError_t cudaEventCreate(cudaEvent_t *event) {
-    // FIXME: implement
-    cerr << "*** Error: cudaEventCreate() not yet implemented!" << endl;
-    return cudaErrorUnknown;
+    Frontend *f = Frontend::GetFrontend();
+    f->AddHostPointerForArguments(event);
+    f->Execute("cudaEventCreate");
+    if(f->Success())
+        *event = *(f->GetOutputHostPointer<cudaEvent_t>());
+    return f->GetExitCode();
 }
 
 extern cudaError_t cudaEventCreateWithFlags(cudaEvent_t *event, int flags) {
-    // FIXME: implement
-    cerr << "*** Error: cudaEventCreateWithFlags() not yet implemented!"
-            << endl;
-    return cudaErrorUnknown;
+    Frontend *f = Frontend::GetFrontend();
+    f->AddHostPointerForArguments(event);
+    f->AddVariableForArguments(flags);
+    f->Execute("cudaEventCreateWithFlags");
+    if(f->Success())
+        *event = *(f->GetOutputHostPointer<cudaEvent_t>());
+    return f->GetExitCode();
 }
 
 extern cudaError_t cudaEventDestroy(cudaEvent_t event) {
-    // FIXME: implement
-    cerr << "*** Error: cudaEventDestroy() not yet implemented!" << endl;
-    return cudaErrorUnknown;
+    Frontend *f = Frontend::GetFrontend();
+    f->AddVariableForArguments(event);
+    f->Execute("cudaEventDestroy");
+    return f->GetExitCode();
 }
 
 extern cudaError_t cudaEventElapsedTime(float *ms, cudaEvent_t start, cudaEvent_t end) {
-    // FIXME: implement
-    cerr << "*** Error: cudaEventElapsedTime() not yet implemented!" << endl;
-    return cudaErrorUnknown;
+    Frontend *f = Frontend::GetFrontend();
+    f->AddHostPointerForArguments(ms);
+    f->AddVariableForArguments(start);
+    f->AddVariableForArguments(end);
+    f->Execute("cudaEventElapsedTime");
+    if(f->Success())
+        *ms = *(f->GetOutputHostPointer<float>());
+    return f->GetExitCode();
 }
 
 extern cudaError_t cudaEventQuery(cudaEvent_t event) {
-    // FIXME: implement
-    cerr << "*** Error: cudaEventQuery() not yet implemented!" << endl;
-    return cudaErrorUnknown;
+    Frontend *f = Frontend::GetFrontend();
+    f->AddVariableForArguments(event);
+    f->Execute("cudaEventQuery");
+    return f->GetExitCode();
 }
 
 extern cudaError_t cudaEventRecord(cudaEvent_t event, cudaStream_t stream) {
-    // FIXME: implement
-    cerr << "*** Error: cudaEventRecord() not yet implemented!" << endl;
-    return cudaErrorUnknown;
+    Frontend *f = Frontend::GetFrontend();
+    f->AddVariableForArguments(event);
+    f->AddVariableForArguments(stream);
+    f->Execute("cudaEventRecord");
+    return f->GetExitCode();
 }
 
 extern cudaError_t cudaEventSynchronize(cudaEvent_t event) {
-    // FIXME: implement
-    cerr << "*** Error: cudaEventSynchronize() not yet implemented!" << endl;
-    return cudaErrorUnknown;
+    Frontend *f = Frontend::GetFrontend();
+    f->AddVariableForArguments(event);
+    f->Execute("cudaEventSynchronize");
+    return f->GetExitCode();
 }
