bcfd48354f13a5dfdbf780fe884d83891eb3c015
Giuseppe Coviello <gcov@pm.me>
2009-11-10 10:30:21 +0100

    implemented opengl routine

--- CudaRt_opengl.cpp
+++ CudaRt_opengl.cpp
@@ -4,57 +4,60 @@
 using namespace std;
 
 extern cudaError_t cudaGLMapBufferObject(void **devPtr, GLuint bufObj) {
-    // FIXME: implement
-    cerr << "*** Error: cudaGlMapBufferObject() not yet implemented!" << endl;
+    cerr << "I'm sorry but it isn't possibile to use OpenGL Interoperability "
+            << "API." << endl << "Giving up ..." << endl;
+    exit(-1);
     return cudaErrorUnknown;
 }
 
 extern cudaError_t cudaGLMapBufferObjectAsync(void **devPtr, GLuint bufObj,
         cudaStream_t stream) {
-    // FIXME: implement
-    cerr << "*** Error: cudaGlMapBufferObjectAsync() not yet implemented!"
-            << endl;
+    cerr << "I'm sorry but it isn't possibile to use OpenGL Interoperability "
+            << "API." << endl << "Giving up ..." << endl;
+    exit(-1);
     return cudaErrorUnknown;
 }
 
 extern cudaError_t cudaGLRegisterBufferObject(GLuint bufObj) {
-    // FIXME: implement
-    cerr << "*** Error: cudaGlRegisterBufferObject() not yet implemented!"
-            << endl;
+    cerr << "I'm sorry but it isn't possibile to use OpenGL Interoperability "
+            << "API." << endl << "Giving up ..." << endl;
+    exit(-1);
     return cudaErrorUnknown;
 }
 
 extern cudaError_t cudaGLSetBufferObjectMapFlags(GLuint bufObj,
         unsigned int flags) {
-    // FIXME: implement
-    cerr << "*** Error: cudaGLSetBufferObjectMapFlags() not yet implemented!"
-            << endl;
+    cerr << "I'm sorry but it isn't possibile to use OpenGL Interoperability "
+            << "API." << endl << "Giving up ..." << endl;
+    exit(-1);
     return cudaErrorUnknown;
 }
 
 extern cudaError_t cudaGLSetGLDevice(int device) {
-    // FIXME: implement
-    cerr << "*** Error: cudaGlSetGLDevice() not yet implemented!" << endl;
+    cerr << "I'm sorry but it isn't possibile to use OpenGL Interoperability "
+            << "API." << endl << "Giving up ..." << endl;
+    exit(-1);
     return cudaErrorUnknown;
 }
 
 extern cudaError_t cudaGLUnmapBufferObject(GLuint bufObj) {
-    // FIXME: implement
-    cerr << "*** Error: cudaGLUnmapBufferObject() not yet implemented!" << endl;
+    cerr << "I'm sorry but it isn't possibile to use OpenGL Interoperability "
+            << "API." << endl << "Giving up ..." << endl;
+    exit(-1);
     return cudaErrorUnknown;
 }
 
 extern cudaError_t cudaGLUnmapBufferObjectAsync(GLuint bufObj,
         cudaStream_t stream) {
-    // FIXME: implement
-    cerr << "*** Error: cudaGLUnmapBufferObjectAsync() not yet implemented!"
-            << endl;
+    cerr << "I'm sorry but it isn't possibile to use OpenGL Interoperability "
+            << "API." << endl << "Giving up ..." << endl;
+    exit(-1);
     return cudaErrorUnknown;
 }
 
 extern cudaError_t cudaGLUnregisterBufferObject(GLuint bufObj) {
-    // FIXME: implement
-    cerr << "*** Error: cudaGLUnregisterBufferObject() not yet implemented!"
-            << endl;
+    cerr << "I'm sorry but it isn't possibile to use OpenGL Interoperability "
+            << "API." << endl << "Giving up ..." << endl;
+    exit(-1);
     return cudaErrorUnknown;
 }
