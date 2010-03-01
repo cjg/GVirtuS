97b3f036493a8aeefda3b75f29014a6ab24bf1b6
Giuseppe Coviello <gcov@pm.me>
2010-03-01 14:27:28 +0100

    updated autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,10 @@
-aclocal
-autoconf
-libtoolize --force
-automake --add-missing
+#!/bin/bash
+
+aclocal || exit $?
+autoconf || exit $?
+libtoolize --force || exit $?
+automake --add-missing --copy || exit $?
 echo ./configure "$@"
 ./configure "$@"
+
+# End Of File
