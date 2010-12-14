0a7782907025f9da8d9ba02f49bb0813299450dd
Giuseppe Coviello <gcov@pm.me>
2010-12-14 12:04:42 +0100

    stripped down old useless contents from ConfigFile

--- util/ConfigFile.h
+++ util/ConfigFile.h
@@ -41,81 +41,10 @@
 #include <map>
 
 /**
- * ConfigFile is the object that parses the XML config file and exposes it to
- * the application as an ordered collection of section(s) and element(s).
+ * ConfigFile is the object that parses the .properties config file
+ * and exposes it to the application as a collection of key(s) and values(s).
  */
 class ConfigFile {
-public:
-    /**
-     * ConfigFile::Element is the object used for storing a configuration
-     * element.
-     */
-    class Element {
-    public:
-        Element(std::string & name);
-        Element(const char * name);
-        Element(ConfigFile::Element & other);
-        virtual ~Element();
-        std::string & GetName() const;
-        bool HasKey(std::string & key);
-        bool HasKey(const char * key);
-        std::string & GetValue(std::string & key) const;
-        std::string & GetValue(const char * key) const;
-        std::string & GetValue(std::string & key,
-            std::string & default_value) const;
-        std::vector<std::string> & GetKeys() const;
-        void SetValue(std::string & key, std::string & value);
-        void SetValue(const char * key, const char * value);
-        void Dump();
-        void Dump(int sectionLevel);
-
-        short GetShortValue(const char * key);
-        short GetShortValueFromOctal(const char * key);
-
-        bool GetBoolValue(const char * key);
-    private:
-        std::string *mpName;
-        std::map<std::string, std::string> * mpContent;
-        std::vector<std::string> * mpContentKeys;
-    };
-
-    /**
-     * ConfigFile::Section is the object used for storing a configuration
-     * section. A Section can have many element(s) and subsection(s).
-     */
-    class Section {
-    public:
-        Section(std::string &name);
-        Section(const char * name);
-        virtual ~Section();
-
-        std::string & GetName() const;
-
-        bool HasElement(std::string & name);
-        std::vector<std::string> & GetElements() const;
-        void AddElement(Element * element);
-        Element & GetElement(std::string & name) const;
-        Element & GetElement(const char * name) const;
-
-        bool HasSection(std::string & name);
-        std::vector<std::string> & GetSections() const;
-        void AddSection(Section * section);
-        Section & GetSection(std::string & name) const;
-        Section & GetSection(const char * name) const;
-
-        void Dump();
-    private:
-        Section();
-        void Initialize();
-        void Dump(int level);
-        std::string *mpName;
-        std::map<std::string, Element *> *mpElements;
-        std::vector<std::string> *mpElementsKeys;
-        std::map<std::string, Section *> * mpSubsections;
-        std::vector<std::string> * mpSubectionsKeys;
-    };
-
-
 public:
     ConfigFile(const char * filename);
     virtual ~ConfigFile();
