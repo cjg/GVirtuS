160d7b1f7b3a9a69b0566e2b1eee2ff26f02a1cb
Carmine Ferraro <carmine.ferraro@uniparthenope.it>
2015-05-26 15:37:11 +0200

    Add typedef for 32-bit support

---
+++ gvirtus/util/gvirtus-type.h
@@ -0,0 +1,7 @@
+#include <stdint.h>
+
+#if defined(__ia64) || defined(__itanium__) || defined(_M_IA64) || defined(__x86_64__) || defined(_M_X64)
+  typedef uint64_t pointer_t;
+#else
+  typedef uint32_t pointer_t; 
+#endif
