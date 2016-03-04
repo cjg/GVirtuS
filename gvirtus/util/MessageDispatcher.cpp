1b9092465bb9b0b245a5c3720a1325b50544712e
Carlo Palmieri <carlo.palmieri@uniparthenope.it>
2016-03-04 15:52:14 +0100

    add MessageDispatcher files

---
+++ gvirtus/util/MessageDispatcher.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   MessageDispatcher.cpp
+ * Author: cpalmieri
+ * 
+ * Created on March 4, 2016, 2:22 PM
+ */
+
+#include "MessageDispatcher.h"
+
+MessageDispatcher::MessageDispatcher() {
+}
+
+MessageDispatcher::MessageDispatcher(const MessageDispatcher& orig) {
+}
+
+MessageDispatcher::~MessageDispatcher() {
+}
+
---
+++ gvirtus/util/MessageDispatcher.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   MessageDispatcher.h
+ * Author: cpalmieri
+ *
+ * Created on March 4, 2016, 2:22 PM
+ */
+
+#ifndef MESSAGEDISPATCHER_H
+#define	MESSAGEDISPATCHER_H
+
+class MessageDispatcher {
+public:
+    MessageDispatcher();
+    MessageDispatcher(const MessageDispatcher& orig);
+    virtual ~MessageDispatcher();
+private:
+
+};
+
+#endif	/* MESSAGEDISPATCHER_H */
+
