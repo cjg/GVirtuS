6b0d476d82c388db938dc5b1ea6f344e70edeb87
Raffaele Montella <raffaele.montella@uniparthenope.it>
2017-01-09 17:34:37 +0100

    Deleted

--- gvirtus.cudadr/config.h
+++ gvirtus.cudadr/config.h
@@ -115,9 +115,6 @@
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "gvirtus-cudadr"
 
-/* Define to the home page for this package. */
-#define PACKAGE_URL ""
-
 /* Define to the version of this package. */
 #define PACKAGE_VERSION "01-beta2"
 
