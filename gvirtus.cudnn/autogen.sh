e0745ad52e8d1635221aaa24c16b178ceb885182
Vincenzo Santopietro <vincenzo.santopietro@studenti.uniparthenope.it>
2017-06-08 14:45:00 +0200

    fixed autogen

--- gvirtus.cudnn/autogen.sh
+++ gvirtus.cudnn/autogen.sh
@@ -4,7 +4,7 @@ aclocal || exit $?
 autoconf || exit $?
 libtoolize --force || exit $?
 automake --add-missing --copy || exit $?
-#echo ./configure "$@" --host=i686-linux-gnu "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32"
-#./configure "$@" "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32"
+echo ./configure "$@"
 ./configure "$@"
+
 # End Of File
