5aee6350fcac7cd60942946c87f0770da94379c9
Antonio Mentone <antonio.mentone.96@gmail.com>
2019-02-21 14:21:55 +0100

    Comment for doxygen
    Property.h: comment for documentation completed

--- core/include/backend/Property.h
+++ core/include/backend/Property.h
@@ -9,25 +9,57 @@
 #include <nlohmann/json.hpp>
 
 namespace gvirtus {
+    /**
+     *
+     */
     class Property {
     public:
+        /**
+         * Default constructor
+         */
         Property() = default;
 
+        /**
+         * Parameterized constructor
+         * @param endpoints: pointer to a vector of Endpoint
+         * @param plugins: pointer to a vector of std::string
+         */
         Property(const std::vector<gvirtus::Endpoint> *endpoints,
                  const std::vector<std::string> *plugins);
 
+        /**
+         * This method is a setter for the class member _endpoints
+         * @param endpoints: pointer to a vector of Endpoint
+         * @return reference to itself (Fluent Interface API)
+         */
         Property &endpoints(const std::vector<gvirtus::Endpoint> *endpoints);
 
+        /**
+         * This method is a getter for the class member _endpoints
+         * @return the reference to vector where Endpoint are saved
+         */
         inline const std::vector<gvirtus::Endpoint> &endpoints() const {
             return _endpoints;
         }
 
+        /**
+         * This method is a setter for the class member _plugins
+         * @param plugins: pointer to a vector of std::string
+         * @return reference to itself (Fluent Interface API)
+         */
         Property &plugins(const std::vector<std::string> *plugins);
 
+        /**
+         * This method is a getter for the class member _plugins
+         * @return the reference to vector where plugins string are saved
+         */
         inline const std::vector<std::string> &plugins() const {
             return _plugins;
         }
 
+        /**
+         * Class destroyer
+         */
         ~Property() {
             _plugins.clear();
             _endpoints.clear();
@@ -38,6 +70,12 @@ namespace gvirtus {
         std::vector<gvirtus::Endpoint> _endpoints;
     };
 
+    /**
+     * This function will be used by nlohmann::json object when we call j.get&lt;Property&lt;().
+     * Without this function the program doesn't know how to build a property object from json object.
+     * @param j: reference to json object which contains the data
+     * @param p: reference to property object to be created
+     */
     inline void from_json(const nlohmann::json &j, Property &p) {
         std::vector<gvirtus::Endpoint> endpoints;
 
