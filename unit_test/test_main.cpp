231b789232cc5acd59db83573ba48921c279d0f9
Antonio Mentone <antonio.mentone.96@gmail.com>
2019-02-22 15:46:06 +0100

    unit_test: main
    this is the main to test the classes

---
+++ unit_test/test_main.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+int main(int argc, char *argv[])
+{
+    testing::InitGoogleTest(&argc, argv);
+    auto value = RUN_ALL_TESTS();
+    return 0;
+}
