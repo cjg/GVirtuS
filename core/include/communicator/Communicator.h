c4f5e599ba3290f62deee5b354d7cc5c9c83e412
Antonio Mentone <antonio.mentone.96@gmail.com>
2019-03-07 14:53:43 +0100

    Communitcator interface has been added

---
+++ core/include/communicator/Communicator.h
@@ -0,0 +1,24 @@
+#ifndef GVIRTUSPP_COMMUNICATOR_H
+#define GVIRTUSPP_COMMUNICATOR_H
+
+#include <communicator/ICommunicator.h>
+#include <memory>
+
+namespace gvirtus {
+    class Communicator {
+    public:
+        Communicator() = default;
+
+        explicit Communicator(const Endpoint &);
+
+        ~Communicator();
+
+        void run() {
+            _ptr_i_communicator->run();
+        }
+
+    private:
+        std::shared_ptr<ICommunicator> _ptr_i_communicator;
+    };
+}
+#endif //GVIRTUSPP_COMMUNICATOR_H
