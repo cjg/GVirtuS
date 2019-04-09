afe4098e3d4fcf46d1e1a6ab443515153107381e
Antonio Mentone <antonio.mentone.96@gmail.com>
2019-04-09 02:19:24 +0200

    Update gitignore and removed unused file

--- gvirtus.cudadr/frontend/frontend.sh
+++ gvirtus.cudadr/frontend/frontend.sh
@@ -12,4 +12,4 @@ cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=${INSTALL_FOLDER} -G "Code
 make
 make install
 
-rm -rf cudart.cbp libcudart.so libcudart.a
+rm -rf cuda.cbp libcuda.so libcuda.a
