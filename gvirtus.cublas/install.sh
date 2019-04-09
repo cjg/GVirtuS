4e78e12752632110faca06b31c4127df7d3c17ca
Antonio Mentone <antonio.mentone.96@gmail.com>
2019-04-09 16:37:18 +0200

    gvirtus-installer beautified

---
+++ gvirtus.cublas/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+INSTALL_FOLDER=$1
+
+cd backend
+bash backend.sh ${INSTALL_FOLDER}
+cd ..
+
+cd frontend
+bash frontend.sh ${INSTALL_FOLDER}
+cd ..
+
+echo
+/bin/echo -e "\e[1;30;102;5mGVIRTUS CUDA BLAS MODULE INSTALLATION COMPLETE!\e[0m"
+echo
+echo
---
+++ gvirtus/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+INSTALL_FOLDER=$1
+
+cd util
+bash util.sh ${INSTALL_FOLDER}
+cd ..
+
+cd backend
+bash backend.sh ${INSTALL_FOLDER}
+cd ..
+
+cd etc
+bash etc.sh ${INSTALL_FOLDER}
+cd ..
+
+cd frontend
+bash frontend.sh ${INSTALL_FOLDER}
+cd ..
+
+echo
+/bin/echo -e "\e[1;30;102;5mGVIRTUS MODULE INSTALLATION COMPLETE!\e[0m"
+echo
+echo
