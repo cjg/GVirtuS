2ae227a577f9795a5b41affe34b3c6bfa5a55d42
Antonio Mentone <antonio.mentone.96@gmail.com>
2019-04-11 02:15:50 +0200

    gvirtus.cudart cmake complete

--- gvirtus.cudart/install.sh
+++ gvirtus.cudart/install.sh
@@ -2,17 +2,11 @@
 
 INSTALL_FOLDER=$1
 
-cd util
-bash util.sh ${INSTALL_FOLDER}
-cd ..
-
-cd backend
-bash backend.sh ${INSTALL_FOLDER}
-cd ..
-
-cd frontend
-bash frontend.sh ${INSTALL_FOLDER}
-cd ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=${INSTALL_FOLDER} \
+      -G "CodeBlocks - Unix Makefiles" -j 4 \
+      .
+make
+make install
 
 echo
 /bin/echo -e "\e[1;30;102mGVIRTUS CUDA RUNTIME MODULE INSTALLATION COMPLETE!\e[0m"
