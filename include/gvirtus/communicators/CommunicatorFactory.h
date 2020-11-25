23edb71da3af4cfdc77aa131e3bebe020f243ad0
Raffaele Montella <raffaele.montella@uniparthenope.it>
2020-11-25 21:40:22 +0100

    Added /home/montella/opt/gvirtus-cuda-9.0 for libs root

--- include/gvirtus/communicators/CommunicatorFactory.h
+++ include/gvirtus/communicators/CommunicatorFactory.h
@@ -5,6 +5,7 @@
 #include <memory>
 #include <utility>
 #include <vector>
+#include <stdlib.h> /* getenv */
 #include "Communicator.h"
 #include "Endpoint.h"
 #include "Endpoint_Tcp.h"
@@ -17,6 +18,8 @@ class CommunicatorFactory {
   get_communicator(std::shared_ptr<Endpoint> end, bool secure = false) {
     std::shared_ptr<common::LD_Lib<Communicator, std::shared_ptr<Endpoint>>> dl;
 
+    std::string gvirtus_home = CommunicatorFactory::getGVirtuSHome();
+
     if (!secure) {
       if (end->protocol() == "tcp" || end->protocol() == "http" ||
           end->protocol() == "oldtcp" ||
@@ -24,7 +27,7 @@ class CommunicatorFactory {
                                       // is better and elegant
         dl = std::make_shared<
             common::LD_Lib<Communicator, std::shared_ptr<Endpoint>>>(
-            std::string{GVIRTUS_HOME} + "/lib/libgvirtus-communicators-" +
+            gvirtus_home + "/lib/libgvirtus-communicators-" +
                 end->protocol() + ".so",
             "create_communicator");
       } else
@@ -33,7 +36,7 @@ class CommunicatorFactory {
                end->protocol() == "wss") {  // in secure supported
       dl = std::make_shared<
           common::LD_Lib<Communicator, std::shared_ptr<Endpoint>>>(
-          std::string{GVIRTUS_HOME} + "/lib/libgvirtus-communicators-" +
+          gvirtus_home + "/lib/libgvirtus-communicators-" +
               end->protocol() + ".so",
           "create_communicator");
     } else {
@@ -43,5 +46,34 @@ class CommunicatorFactory {
     dl->build_obj(end);
     return dl;
   }
+
+ private:
+   static std::string getEnvVar(std::string const &key) {
+     char *val = getenv(key.c_str());
+     return val == NULL ? std::string("") : std::string(val);
+   }
+
+   static std::string getGVirtuSHome() {
+     std::string gvirtus_home = CommunicatorFactory::getEnvVar("GVIRTUS_HOME");
+     return gvirtus_home;
+   }
+
+   static std::string getConfigFile() {
+     // Get the GVIRTUS_CONFIG environment varibale
+     std::string config_path = CommunicatorFactory::getEnvVar("GVIRTUS_CONFIG");
+   
+     // Check if the configuration file is defined
+     if (config_path == "" ) {
+
+       // Check if the configuration file is in the GVIRTUS_HOME directory
+       config_path = CommunicatorFactory::getEnvVar("GVIRTUS_HOME")+"/etc/properties.json";
+
+       if (config_path == "") {
+         // Finally consider the current directory
+         config_path = "./properties.json";
+       }
+     }
+     return config_path;
+   }
 };
 }  // namespace gvirtus::communicators
